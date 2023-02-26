86f75837df925cbcc0f892dd0e94bfd8fca5d2cc
minhvo <minhvo4398@gmail.com>
2023-02-26 19:50:00 +0700

    Formatting URL String to retrieve code using java method

--- src/demo/oAuthTest.java
+++ src/demo/oAuthTest.java
@@ -1,12 +1,36 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import io.restassured.path.json.JsonPath;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import static io.restassured.RestAssured.given;
 
 public class oAuthTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+    /*
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://accounts.google.com/o/oauth2/v2/auth/identifier?scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email&auth_url=https%3A%2F%2Faccounts.google.com%2Fo%2Foauth2%2Fv2%2Fauth&client_id=692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com&response_type=code&redirect_uri=https%3A%2F%2Frahulshettyacademy.com%2FgetCourse.php&service=lso&o2v=2&flowName=GeneralOAuthFlow");
+        driver.findElement(By.xpath("//input[@type='email']")).sendKeys("minhvo4398@gmail.com");
+        driver.findElement(By.xpath("//input[@type='email']")).sendKeys(Keys.ENTER);
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//input[@type='password']")).sendKeys("Thienthan@040398");
+        driver.findElement(By.xpath("//input[@type='email']")).sendKeys(Keys.ENTER);
+        Thread.sleep(3000);
+        String url = driver.getCurrentUrl();
+*/
 
-        String asscessTokenResponse = given()
-                .queryParam("code", "")
+        String url = "https://rahulshettyacademy.com/getCourse.php?code=4%2F0AWtgzh5F0tMN0YZXbdURY1Ltota14XX___MjLivgqebjKoeqIPtHJ5hzHVMJd0vPYMspmw&scope=email+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+openid&authuser=0&prompt=consent";
+
+        String partialCode = url.split("code=")[1];
+        String code = partialCode.split("&scope")[0];
+        System.out.println(code);
+
+
+        String asscessTokenResponse = given().urlEncodingEnabled(false)
+                .queryParam("code", code)
                 .queryParam("client_id", "692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com")
                 .queryParam("client_secret", "erZOWM9g3UtwNRj340YYaK_W")
                 .queryParam("https://rahulshettyacademy.com/getCourse.php")
